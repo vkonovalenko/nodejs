2b27181720738592989013b50472544915d0a073
Vyacheslav <viacheslav1991@gmail.com>
2017-02-11 23:12:24 +0200

    refactoring

--- http/crons/setMeetingLocation.js
+++ http/crons/setMeetingLocation.js
@@ -5,17 +5,15 @@ schedule.scheduleJob(Config.get('cron_users_meeting'), function () {
     let client2 = null;
     Model.get('Meeting').findAll({where: {status: 1}}).then(function(meetings) {
         meetings.forEach(function (meeting, k) {
+            client1 = Socket.clients(meeting.userFrom);
+            client2 = Socket.clients(meeting.userTo);
             if (Date.now() <= (new Date(meeting.expiredAt)).getTime()) {
-                client1 = Socket.clients(meeting.userFrom);
-                client2 = Socket.clients(meeting.userTo);
                 if (client1 && client2) {
                     client1.send(Response.socket('do_set_location', {}));
                     client2.send(Response.socket('do_set_location', {}));
                 }
             } else {
                 Model.get('Meeting').update({status: 4}, {where: {id: meeting.id}});
-                client1 = Socket.clients(meeting.userFrom);
-                client2 = Socket.clients(meeting.userTo);
                 if (client1) {
                     client1.send(Response.socket('meeting_expired', {meetingId: meeting.id}));
                 }
