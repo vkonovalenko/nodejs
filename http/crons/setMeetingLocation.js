6d6b66278db3038f4c3a4340277136e397f1a6ee
Vyacheslav <viacheslav1991@gmail.com>
2017-02-11 23:10:22 +0200

    meeting expiring cron

--- http/crons/setMeetingLocation.js
+++ http/crons/setMeetingLocation.js
@@ -5,11 +5,23 @@ schedule.scheduleJob(Config.get('cron_users_meeting'), function () {
     let client2 = null;
     Model.get('Meeting').findAll({where: {status: 1}}).then(function(meetings) {
         meetings.forEach(function (meeting, k) {
-            client1 = Socket.clients(meeting.userFrom);
-            client2 = Socket.clients(meeting.userTo);
-            if (client1 && client2) {
-                client1.send(Response.socket('do_set_location', {}));
-                client2.send(Response.socket('do_set_location', {}));
+            if (Date.now() <= (new Date(meeting.expiredAt)).getTime()) {
+                client1 = Socket.clients(meeting.userFrom);
+                client2 = Socket.clients(meeting.userTo);
+                if (client1 && client2) {
+                    client1.send(Response.socket('do_set_location', {}));
+                    client2.send(Response.socket('do_set_location', {}));
+                }
+            } else {
+                Model.get('Meeting').update({status: 4}, {where: {id: meeting.id}});
+                client1 = Socket.clients(meeting.userFrom);
+                client2 = Socket.clients(meeting.userTo);
+                if (client1) {
+                    client1.send(Response.socket('meeting_expired', {meetingId: meeting.id}));
+                }
+                if (client2) {
+                    client2.send(Response.socket('meeting_expired', {meetingId: meeting.id}));
+                }
             }
         });
     });
