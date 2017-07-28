b4fd1ba7efd2c5aebbb09bb65b9402a709c0224d
Vyacheslav Konovalenko <vyacheslav.konovalenko@ideus.biz>
2017-07-28 17:43:17 +0300

    fixes: write to the database wasOnline datetime

--- sockets/events/Close.js
+++ sockets/events/Close.js
@@ -1,13 +1,20 @@
 "use strict";
 
+var moment = require("moment");
+
 class Close {
     
     static handle(msg) {
         if (Helper.isVar(this.user_id)) {
             let ws = Socket.clients(this.user_id);
-            Socket.sendToFriends(ws, 'online_status', {id: this.user_id, status: false});
+            Socket.sendToFriends(ws, 'online_status', {id: this.user_id, status: false, wasOnline: this.wasOnline});
             App.geo().removeLocation(this.user_id);
             Socket.del(this.user_id);
+			
+			var now = moment();
+			const dateFormatted = now.format('YYYY-MM-DD HH:mm:ss');
+			
+			Model.get('User').update({wasOnline: dateFormatted}, {where: {id: this.user_id}});
         }
     }
     
