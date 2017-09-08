0beb8462d8a5b34b5a17df00b92494e331f42bd8
Vyacheslav Konovalenko <vyacheslav.konovalenko@ideus.biz>
2017-09-08 13:42:46 +0300

    bug fixes - wasOnline date

--- sockets/events/Close.js
+++ sockets/events/Close.js
@@ -7,12 +7,13 @@ class Close {
     static handle(msg) {
         if (Helper.isVar(this.user_id)) {
             let ws = Socket.clients(this.user_id);
-            Socket.sendToFriends(ws, 'online_status', {id: this.user_id, status: false, wasOnline: this.wasOnline});
-            App.geo().removeLocation(this.user_id);
-            Socket.del(this.user_id);
 			
 			var now = moment();
 			const dateFormatted = now.format('YYYY-MM-DD HH:mm:ss');
+			
+            Socket.sendToFriends(ws, 'online_status', {id: this.user_id, status: false, wasOnline: dateFormatted});
+            App.geo().removeLocation(this.user_id);
+            Socket.del(this.user_id);
 			
 			Model.get('User').update({wasOnline: dateFormatted}, {where: {id: this.user_id}});
         }
