f4ed99da83cd52e611b05a00d2fdf7aafd4d28c4
Vyacheslav Konovalenko <vyacheslav.konovalenko@ideus.biz>
2017-09-11 13:57:37 +0300

    optimization

--- http/crons/setLocationRequest.js
+++ http/crons/setLocationRequest.js
@@ -2,13 +2,18 @@ let schedule = require('node-schedule');
 
 schedule.scheduleJob(Config.get('cron_users_set_location'), function () {
     const clients = Socket.clients();
-    let loginedClient = null;
-    if (clients) {
-        Object.keys(clients).forEach(function(user_id) {
-            loginedClient = Socket.clients(user_id);
-            if (loginedClient) {
-                loginedClient.send('{"action": "do_set_location"}');
-            }
-        });
-    }
+	if (clients) {
+		
+		let async = require('async');
+		function sendLocationRequest(user_id, callback) {
+			process.nextTick(function () {
+				let loginedClient = Socket.clients(user_id);
+				if (loginedClient) {
+					loginedClient.send('{"action": "do_set_location"}');
+				}
+				callback(null, user_id);
+			});
+		}
+		async.map(Object.keys(clients), sendLocationRequest)
+	}
 });
