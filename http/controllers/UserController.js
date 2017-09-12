4d94aea12f6c9d53dbd7936b21b824c98f0faefc
Vyacheslav Konovalenko <vyacheslav.konovalenko@ideus.biz>
2017-09-12 12:57:17 +0300

    file uploading optimizations

--- http/controllers/UserController.js
+++ http/controllers/UserController.js
@@ -22,13 +22,6 @@ class UserController {
 //    const uuidV4 = require('uuid/v4');
     static uploadPhotos(request, response) {
 		
-		async function executeParallelAsyncTasks () {  
-		  const [ valueA, valueB, valueC ] = await Promise.all([ functionA(), functionB(), functionC() ]);
-		  doSomethingWith(valueA);
-		  doSomethingElseWith(valueB);
-		  doAnotherThingWith(valueC);
-		};
-		
 		console.log('UPLOAD ACTION');
 		let req = request.query;
 		console.log(request);
@@ -44,60 +37,63 @@ class UserController {
 						let path = null;
 						let fileName = null;
 						let src = null;
-						let formattedFile = null;
 						let formattedFiles = [];
-						let filesLength = request.files.length - 1;
+						//let filesLength = request.files.length - 1;
 						
-						function sendResponse(formattedFiles) {
+						function sendResponse(err, formattedFiles) {
 							console.log('-------------------------------');
 							console.log(formattedFiles);
-							response.send(Response.http({photos: formattedFiles}, 'files_uploaded'));
+							
+							if (formattedFiles.length) {
+								response.send(Response.http({photos: formattedFiles}, 'files_uploaded'));
+							}
 						}
 						
-						request.files.forEach(function(file, i) {
-							ext = file.originalname.split(".").pop().toLowerCase();
-							path = file.path;
+						let async = require('async');
+						function uploadFile(file, callback) {
+							process.nextTick(function() {
+								
+								ext = file.originalname.split(".").pop().toLowerCase();
+								path = file.path;
 
-							if (Helper.inArray(ext, UserController.exts())) {
-								fileName = uuidV4() + '.' + ext;
-								fs.rename(Config.get('app_path') + path, Config.get('app_path') + 'public/uploads/' + fileName, function(err) {
-									if ( err ) {
-										console.log('ERROR: ' + err);
-									}
-								});
-								src = "/" + fileName;
-								file = {src: src, userId: user.id};
-
-								Model.get('UserPhoto').create(file).then(function(created_file) {
-									formattedFile = {
-										id: created_file.id,
-										src: Config.get('image_url') + created_file.src
-									};
-									
-									formattedFiles.push(formattedFile);
-									
-									if(Helper.isVar(req.avatar) == true) {
-										Model.get('User').update({avatar: created_file.src}, {where: {id: user.id}});
-										let ws = Socket.clients(user.id);
-										if (ws) {
-											console.log('AVATAR UPDATING.............');
-											ws.avatar = created_file.src;
-											
-											Response.socket(ws, 'avatar', {avatar: created_file.src});
+								if (Helper.inArray(ext, UserController.exts())) {
+									fileName = uuidV4() + '.' + ext;
+									fs.rename(Config.get('app_path') + path, Config.get('app_path') + 'public/uploads/' + fileName, function(err) {
+										if ( err ) {
+											console.log('ERROR: ' + err);
 										}
-									}
-									
-									if (filesLength == i) {
-										sendResponse(formattedFiles);
-									}
-									
-								}, function(error) {
-									console.log(error);
-								});
-							} else {
-								response.send(Response.http({}, 'invalid_file_format'));
-							}
-						});
+									});
+									src = "/" + fileName;
+									file = {src: src, userId: user.id};
+
+									Model.get('UserPhoto').create(file).then(function(created_file) {
+
+										formattedFiles.push({
+											id: created_file.id,
+											src: Config.get('image_url') + created_file.src
+										});
+
+										if(Helper.isVar(req.avatar) == true) {
+											Model.get('User').update({avatar: created_file.src}, {where: {id: user.id}});
+											let ws = Socket.clients(user.id);
+											if (ws) {
+												ws.avatar = created_file.src;
+												formattedFiles = [];
+												Response.socket(ws, 'avatar', {avatar: created_file.src});
+											}
+										}
+									}, function(error) {
+										console.log(error);
+									});
+								} else {
+									response.send(Response.http({}, 'invalid_file_format'));
+								}
+								
+								callback(null, file);
+							});
+						}
+						
+						async.map(request.files, uploadFile, sendResponse);
 						
 					} else {
 						response.send(Response.http({}, 'no_files_to_upload'));
